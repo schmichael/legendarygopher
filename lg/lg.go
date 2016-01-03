da70df616bb1a73dc6300a3d5bad4d7782d1d662
Michael Schurter <schmichael@lytics.io>
2016-01-02 22:26:16 -0800

    Support figures listing and figure details

--- lg/lg.go
+++ lg/lg.go
@@ -22,8 +22,9 @@ type DFWorld struct {
 	Regions            []*Region            `xml:"regions>region"`
 	UndergroundRegions []*UndergroundRegion `xml:"underground_regions>underground_region"`
 	Sites              []*Site              `xml:"sites>site"`
-	Artifacts          []*Artifact          `xml:"artifacts>artifact"`
-	Figures            []*Figure            `xml:"historical_figures>historical_figure"`
+	siteidx            map[int]*Site
+	Artifacts          []*Artifact `xml:"artifacts>artifact"`
+	Figures            []*Figure   `xml:"historical_figures>historical_figure"`
 	figidx             map[int]*Figure
 
 	// useless?
@@ -34,6 +35,11 @@ type DFWorld struct {
 }
 
 func (w *DFWorld) init() {
+	w.siteidx = make(map[int]*Site, len(w.Sites))
+	for _, s := range w.Sites {
+		w.siteidx[s.ID] = s
+	}
+
 	w.figidx = make(map[int]*Figure, len(w.Figures))
 	for _, f := range w.Figures {
 		w.figidx[f.ID] = f
@@ -42,6 +48,42 @@ func (w *DFWorld) init() {
 
 func (w *DFWorld) Figure(id int) *Figure {
 	return w.figidx[id]
+}
+
+func (w *DFWorld) Site(id int) *Site {
+	return w.siteidx[id]
+}
+
+func (w *DFWorld) FigureEvents(id int) <-chan *Event {
+	out := make(chan *Event, 100)
+	go func() {
+		defer close(out)
+		for _, e := range w.Events {
+			if e.FigureID == id || e.SlayerFigureID == id {
+				out <- e
+			}
+		}
+	}()
+	return out
+}
+
+func (w *DFWorld) RenderEvent(e *Event) string {
+	switch e.Type {
+	case "destroyed site":
+		return fmt.Sprintf("Site %d for Civ %d destroyed by Civ %d", e.SiteID, e.DefenderCivID, e.AttackerCivID)
+	case "change hf state":
+		if e.SiteID != -1 {
+			return fmt.Sprintf("%s %s %s", w.Figure(e.FigureID), e.State, w.Site(e.SiteID))
+		}
+		return fmt.Sprintf("%s %s", w.Figure(e.FigureID), e.State)
+	case "hf died":
+		if slayer := w.Figure(e.SlayerFigureID); slayer != nil {
+			return fmt.Sprintf("%s slayed by %s", w.Figure(e.FigureID), slayer)
+		}
+		return fmt.Sprintf("%s died", w.Figure(e.FigureID))
+	default:
+		return fmt.Sprint("Event %d in %d (unknown type %q)", e.ID, e.Year, e.Type)
+	}
 }
 
 func (w *DFWorld) String() string {
@@ -133,11 +175,18 @@ type Entity struct {
 type Event struct {
 	ID             int    `xml:"id"`
 	Year           int    `xml:"year"`
-	Type           string `xml:"type"`
+	Seconds        int    `xml:"seconds72"`
+	Type           string `xml:"type"`            // "change hf state","hf died","destroyed_site"
+	AttackerCivID  int    `xml:"attacker_civ_id"` // used for type=destroyed_site
+	DefenderCivID  int    `xml:"defender_civ_id"` // used for type=destroyed_site
 	FigureID       int    `xml:"hfid"`
-	State          string `xml:"state"` // settled,wandering
+	SlayerFigureID int    `xml:"slayer_hfid"`
+	SlayerItemID   int    `xml:"slayer_item_id"`
+	State          string `xml:"state"`       // visiting,settled,wandering
+	SiteCivID      int    `xml:"site_civ_id"` // used for type=destroyed_site
 	SiteID         int    `xml:"site_id"`
 	SubregionID    int    `xml:"subregion_id"`
 	FeatureLayerID int    `xml:"feater_layer_id"`
 	Coords         string `xml:"coords"`
+	Cause          string `xml:"cause"`
 }
--- web.go
+++ web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -14,6 +15,8 @@ var (
 	artifactst = template.Must(template.New("artifacts").Parse(string(MustAsset("assets/templates/artifacts.html"))))
 	entitiest  = template.Must(template.New("entities").Parse(string(MustAsset("assets/templates/entities.html"))))
 	eventst    = template.Must(template.New("events").Parse(string(MustAsset("assets/templates/events.html"))))
+	figurest   = template.Must(template.New("figures").Parse(string(MustAsset("assets/templates/figures.html"))))
+	figuret    = template.Must(template.New("figure").Parse(string(MustAsset("assets/templates/figure.html"))))
 )
 
 type server struct {
@@ -23,17 +26,26 @@ type server struct {
 //go:generate go-bindata assets/...
 func runserver(bind string, w *lg.DFWorld) {
 	s := &server{World: w}
-	http.HandleFunc("/", s.templateHandler(indext))
-	http.HandleFunc("/artifacts", s.templateHandler(artifactst))
-	http.HandleFunc("/entities", s.templateHandler(entitiest))
-	http.HandleFunc("/events", s.templateHandler(eventst))
-	http.HandleFunc("/assets/", s.assetHandler)
+	http.HandleFunc("/", wrap(s.listHandler(indext)))
+	http.HandleFunc("/artifacts", wrap(s.listHandler(artifactst)))
+	http.HandleFunc("/entities", wrap(s.listHandler(entitiest)))
+	http.HandleFunc("/events", wrap(s.listHandler(eventst)))
+	http.HandleFunc("/figures", wrap(s.listHandler(figurest)))
+	http.HandleFunc("/figures/", wrap(s.figureHandler))
+	http.HandleFunc("/assets/", wrap(s.assetHandler))
 	if err := http.ListenAndServe(bind, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (s *server) templateHandler(t *template.Template) http.HandlerFunc {
+func wrap(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer log.Printf(r.URL.Path)
+		f(w, r)
+	}
+}
+
+func (s *server) listHandler(t *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := t.Execute(w, s); err != nil {
 			log.Printf("error executing template %s: %v", t.Name(), err)
@@ -41,6 +53,32 @@ func (s *server) templateHandler(t *template.Template) http.HandlerFunc {
 			w.Write([]byte(err.Error()))
 			return
 		}
+	}
+}
+
+func (s *server) figureHandler(w http.ResponseWriter, r *http.Request) {
+	id := 0
+	if _, err := fmt.Sscanf(r.URL.Path, "/figures/%d", &id); err != nil {
+		log.Printf("error getting figure id from %q: %v", r.URL.Path, err)
+		w.WriteHeader(404)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fig := s.World.Figure(id)
+	if fig == nil {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "not found: figure %d", id)
+		return
+	}
+	context := struct {
+		Figure *lg.Figure
+		World  *lg.DFWorld
+	}{fig, s.World}
+	if err := figuret.Execute(w, context); err != nil {
+		log.Printf("error executing template %s: %v", figuret.Name(), err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
 	}
 }
 
